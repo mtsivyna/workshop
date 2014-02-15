355cd6e8659eba7bc7bb29fec6560eb85137a3c3
mtsivyna <max_3d@mail.ru>
2014-02-15 12:52:25 +0200

    first commit

---
+++ src/test/java/com/workshop/FirstTest.java
@@ -0,0 +1,11 @@
+package com.workshop;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mtsivyna
+ * Date: 2/15/14
+ * Time: 12:51 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FirstTest {
+}
